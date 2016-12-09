b3b3ce5daac385ebd688f0c8b18c032daf889a06
elindgr1 <elindgr1@msudenver.edu>
2016-12-09 13:44:04 -0700

    Add files via upload

--- board.rb
+++ board.rb
@@ -4,35 +4,122 @@ class Board
   EMPTY_POS = ' '
 
   def initialize(starting_player)
-    # implement
+    @current_player = current_player
+    @board = Array.new(BOARD_MAX_INDEX + 1) {
+      Array.new(BOARD_MAX_INDEX + 1) { EMPTY_POS}
+    }
+  end
   end
 
   def display
-    # implement
+    puts "+- - - - - -+"
+    for row in 0..BOARD_MAX_INDEX
+      # print has to be used when we don't want to output a line break
+      print "| "
+      for col in 0..BOARD_MAX_INDEX
+        s = @board[row][col]
+        if s == EMPTY_POS
+          print col + (row * 3) + 1
+        else
+          print s
+        end
+        print " | "
+      end
+      puts "\n+- - - - - -+"
+    end
   end
 
   def board_full
-    # implement
+    for row in 0..BOARD_MAX_INDEX
+      for col in 0..BOARD_MAX_INDEX
+        if @board[row][col] == EMPTY_POS
+          return false
+        end
+      end
+    end
+    # Since we found no open positions, the board is full
+    return true
   end
 
   def winner
-    # implement
+    winner = winner_rows()
+    if winner
+      return winner
+    end
+    winner = winner_cols()
+    if winner
+      return winner
+    end
+    winner = winner_diagnols()
+    if winner
+      return winner
+    end
+    # No winners
+    return
   end
 
   def winner_cols
-    # implement
+    for col_index in 0..BOARX_MAX_INDEX
+      first_symbol = @board[0][col_index]
+      for row_index in 1..BOARD_MAX_INDEX
+        if first_symbol != @board[row_index][col_index]
+          break
+        elsif row_index == BOARD_MAX_INDEX and first_symbol != EMPTY_POS
+          return first_symbol
+        end
+      end
+      return
   end
 
   def winner_rows
-    # implement
+    for row_index in 0..BOARD_MAX_INDEX
+      first_symbol = @board[row_index][0]
+      for col_index in 1..BOARD_MAX_INDEX
+        if first_symbol != @board[row_index][col_index]
+          break
+        elsif col_index == BOARD_MAX_INDEX and first symbol != EMPTY_POS
+          return first_symbol
+        end
+      end
+    end
+    return
   end
 
   def winner_diagonals
-    # implement
+    first_symbol = @board[0][0]
+    for index in 1..BOARD_MAX_INDEX
+      if first symbol != @board[index][index]
+        break
+      elsif index == BOARD_MAX_INDEX and first_symbol != EMPTY_POS
+        return first_symbol
+      end
+    end
+    first_symbol = @board[0][BOARD_MAX_INDEX]
+    row_index = 0
+    col_index = BOARD_MAX_INDEX
+    while row_index < BOARD_MAX_INDEX
+      row_index = row_index + 1
+      col_index = col_index - 1
+      if first_symbol != @board[row_index][col_index]
+        break
+      elsif row_index == BOARD_MAX_INDEX and first_symbol != EMPTY_POS
+        return first_symbol
+      end
+    end
+    return
   end
 
   def validate_position(row, col)
-    # implement
+    if row <= @board.size and col <= @board.size
+      if (@board[row][col] == EMPTY_POS) then
+        return true
+      else
+        puts "That position is occupied."
+      end
+    else
+      puts "Invalid position."
+    end
+    return false
   end
 
   def fill_position(row, col, player)
--- game.rb
+++ game.rb
@@ -4,19 +4,68 @@ class Game
   PLAYERS = %w{X O}
 
   def initialize(starting_player)
-    # implement
+    @current_player = current_player
+    @board = Array.new(BOARD_MAX_INDEX + 1) {
+      Array.new(BOARD_MAX_INDEX + 1) { EMPTY_POS}
+    }
+  end
   end
 
   def play
-    # implement
+    print_initial_instruction
+    interact_with_human
+    play if continue_to_play?
   end
+
+def print_initial_instruction
+  puts "Welcome to a Tic-Tac-Toe Game!\nYou are playing against the computer. Try to win."
+
+def continue_to_play?
+  print "Would you like to play again? (Y or N): "
+  if STDIN.gets.chomp() =~ /Y|y/
+    @board = Tic_Tac_Toe.new
+    return true
+  end
+  false
+end
+
 
   def ask_player_for_move
-    # implement
+    if current_player == COMPUTER_PLAYER
+      computer_move(current_player)
+    else
+      human_move(current_player)
+    end
   end
 
-  def next_player
-    # implement
+  def computer_move(current_player)
+    row = -1
+    col = -1
+    found = "F"
+
+    check_rows(COMPUTER_PLAYER, found)
+    check_cols(COMPUTER_PLAYER, found)
+    check_diagonals(COMPUTER_PLAYER, found)
+
+    check_rows(HUMAN_PLAYER, found)
+    check_cols(HUMAN_PLAYER, found)
+    check_diagonals(HUMAN_PLAYER, found)
+
+    if found == "F"
+      if @board[1][1] == EMPTY_POS
+        row = 1
+        col = 1
+        @board[row][col] = current_player
+      elsif available_corner
+        pick_corner(current_player)
+      else
+        until validate_position(row, col)
+          row =  rand(@board.size)
+          col = rand (board.size)
+        end
+        @board[row][col] = current_player
+      end
+    end
   end
 
 end
